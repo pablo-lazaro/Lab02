204d50fcb45cb098611dc7b560eeab383d0390a8
Pablo <pablo.lazaro.ojeda@gmail.com>
2026-03-11 15:59:46 +0100

    Reseted the project

--- main.py
+++ main.py
@@ -2,35 +2,24 @@ import translator as tr
 
 t = tr.Translator()
 
+
 while(True):
 
     t.printMenu()
 
-    dizionario = t.loadDictionary("dictionary.txt")
+    t.loadDictionary("filename.txt")
 
-    txtIn = int(input())
+    txtIn = input()
 
     # Add input control here!
 
     if int(txtIn) == 1:
-        print("Ok, quale parola vuoi aggiungere? ")
-        parola_da_aggiungere = input().lower()
-        t.handleAdd(parola_da_aggiungere)
-        print("Aggiunta!")
-
+        print()
+        txtIn = input()
+        pass
     if int(txtIn) == 2:
-
-        print("Ok, quale traduzione devo cercare?")
-        parola_da_cercare = input().lower()
-        traduzione = t.handleTranslate(parola_da_cercare)
-        print(traduzione)
-
+        pass
     if int(txtIn) == 3:
         pass
     if int(txtIn) == 4:
-        print(dizionario)
-
-    break
-    # commit
-
-# una parola --> multiple traduzioni
+        break
--- translator.py
+++ translator.py
@@ -1,55 +1,27 @@
 class Translator:
 
     def __init__(self):
-        self.dizionario = {}
+        pass
 
     def printMenu(self):
         # 1. Aggiungi nuova parola
         # 2. Cerca una traduzione
         # 3. Cerca con wildcard
         # 4. Exit
-        print("\n" + "-"*20)
-        print("1. Aggiungi nuova parola")
-        print("2. Cerca una Traduzione")
-        print("3. Cerca con wildcard")
-        print("4. Stampa tutto il dizionario")
-        print("5. Exit")
-        print("-"*20)
-
+        pass
 
     def loadDictionary(self, dict):
         # dict is a string with the filename of the dictionary
-        with open(dict, "r", encoding="utf-8") as file:
-            for riga in file:
-                riga_pulita = riga.strip().lower()
-                if riga_pulita != "":
-                    self.handleAdd(riga_pulita)
-
-        return self.dizionario
-
+        pass
 
     def handleAdd(self, entry):
         # entry is a tuple <parola_aliena> <traduzione1 traduzione2 ...>
-        parti = entry.split(" ")
-        parola_aliena = parti[0]
-        nuove_traduzioni = parti[1:]
-
-        # Se parola non esiste, creo lista vuota
-        if parola_aliena not in self.dizionario.keys():
-            self.dizionario[parola_aliena] = []
-        # Aggiungo traduzioni alla lista (Es 2)
-        for tr in nuove_traduzioni:
-            if tr not in self.dizionario[parola_aliena]:
-                self.dizionario[parola_aliena].appenf(tr)
+        pass
 
     def handleTranslate(self, query):
         # query is a string <parola_aliena>
-        if query in self.dizionario:
-            return self.dizionario[query]
+        pass
 
     def handleWildCard(self,query):
         # query is a string with a ? --> <par?la_aliena>
-        pass
-
-    def handleMultipleTranslations(self, parole):
         pass
